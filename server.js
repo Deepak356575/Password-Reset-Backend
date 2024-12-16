ebc38fd1315a436710b590acbdb26c42724223dc
Deepak356575 <deepak356575@gmail.com>
2024-12-16 13:32:25 +0530

    updated

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: [
-        'http://localhost:3000',
         'https://ozbourne-pass-reset.netlify.app'
     ],
     credentials: true,
